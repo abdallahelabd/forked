0262fc8b7089f8c039e2b46194ff5f6edebd9b32
abdallahelabd <70246130+abdallahelabd@users.noreply.github.com>
2025-04-07 19:03:48 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,22 @@
-// src/firebaseConfig.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBsQCz6d18CZtZQKD1NH2rNzrjiiwqLThM",
-  authDomain: "abdallah-bio.firebaseapp.com",
-  projectId: "abdallah-bio",
-  storageBucket: "abdallah-bio.firebasestorage.app",
-  messagingSenderId: "598789674277",
-  appId: "1:598789674277:web:18c136dd37f03b2f6a76cc",
-  measurementId: "G-88CM5NQ696"
+  apiKey: "AIzaSyCvJp9MjJ3CJGDcM1dj2U0LYBCtdc5BBmk",
+  authDomain: "abdallahbio-18d4c.firebaseapp.com",
+  databaseURL: "https://abdallahbio-18d4c-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "abdallahbio-18d4c",
+  storageBucket: "abdallahbio-18d4c.firebasestorage.app",
+  messagingSenderId: "1059962976137",
+  appId: "1:1059962976137:web:5e60b5af318796e4b35358",
+  measurementId: "G-GYD479RY6M"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
