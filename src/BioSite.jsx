e2c03849055be8995dac3822d13a149b0116e0f6
abdallahelabd <70246130+abdallahelabd@users.noreply.github.com>
2025-04-07 23:31:24 +0200

    Update BioSite.jsx

--- src/BioSite.jsx
+++ src/BioSite.jsx
@@ -10,7 +10,7 @@ const firebaseConfig = {
   authDomain: "abdallahbio-18d4c.firebaseapp.com",
   databaseURL: "https://abdallahbio-18d4c-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "abdallahbio-18d4c",
-  storageBucket: "abdallahbio-18d4c.firebasestorage.app",
+  storageBucket: "abdallahbio-18d4c.appspot.com",
   messagingSenderId: "1059962976137",
   appId: "1:1059962976137:web:5e60b5af318796e4b35358",
   measurementId: "G-GYD479RY6M"
